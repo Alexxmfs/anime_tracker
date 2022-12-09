a2f333cf1c2dec3a64fa9e640638728e9694332c
Alexxmfs <alexxmfs@hotmail.com>
2022-12-09 16:37:39 -0300

    building api anime

---
+++ src/App.vue
@@ -0,0 +1,87 @@
+<script setup>
+import { ref, computed, onMounted } from 'vue'
+
+const query = ref('')
+const my_anime = ref([])
+const search_results = ref([])
+
+const my_anime_asc = computed(() => {
+  return my_anime.value.sort((a, b) => {
+    return a.title.localeCompare(b.title)
+  })
+})
+
+const searchAnime = () => {
+  const url = `https://api.jikan.moe/v4/anime?q=${query.value}`
+  fetch(url)
+    .then(res => res.json())
+    .then(res => {
+          search_results.value = res.data
+    })
+}
+
+const handleInput = e => {
+  if (!e.target.value) {
+        search_results.value = []
+  }
+}
+
+const addAnime = anime => {
+  search_results.value = []
+  query.value = ''
+
+  my_anime.value.pust({
+      id: anime.mal_id,
+      title: anime.title,
+      image: anime.images.jpg.image_url,
+      total_episodes: anime.total_episodes,
+      watched_episodes: 0  
+  })
+
+  localStorage.setItem('my_anime', JSON.stringify(my_anime.value))
+}
+
+const increaseWatch = anime => {
+  anime.watched_evolutions++
+  localStorage.setItem('my_anime', JSON.stringify(my_anime.value))
+}
+
+const decreaseWatch = anime => {
+  anime.watched_evolutions--
+  localStorage.setItem('my_anime', JSON.stringify(my_anime.value))
+}
+
+onMounted(() => {
+  my_anime.value = JSON.parse(localStorage.getItem('my_anime')) || []
+})
+</script>
+
+<template>
+  <main>
+    <h1>My Anime Tracker</h1>
+
+    <form @submit.prevent="searchAnime">
+        <input
+            type="text"
+            placeholder="Search for an anime..."
+            v-model="query"
+            @input="handleInput"
+             />
+             <button type="submit">Search</button>
+    </form>
+
+    <div class="results" v-if="search_results.length > 0">
+          <div class="result" v-for="anime in search_results">
+            <img :src="anime.images.jpg.image_url" />
+            <div class="details">
+              <h3>{{ anime.title }}</h3>
+            </div>
+
+          </div>
+    </div>
+  </main>
+</template>
+
+<style>
+
+</style>
