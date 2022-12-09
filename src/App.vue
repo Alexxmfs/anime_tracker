43db6c27cfdcd7067861c7aded62680c26f55576
Alexxmfs <alexxmfs@hotmail.com>
2022-12-09 19:35:06 -0300

    building app.vue.

--- src/App.vue
+++ src/App.vue
@@ -30,11 +30,11 @@ const addAnime = (anime) => {
   search_results.value = [];
   query.value = "";
 
-  my_anime.value.pust({
+  my_anime.value.push({
     id: anime.mal_id,
     title: anime.title,
     image: anime.images.jpg.image_url,
-    total_episodes: anime.total_episodes,
+    total_episodes: anime.episodes,
     watched_episodes: 0,
   });
 
@@ -42,12 +42,12 @@ const addAnime = (anime) => {
 };
 
 const increaseWatch = (anime) => {
-  anime.watched_evolutions++;
+  anime.watched_episodes++;
   localStorage.setItem("my_anime", JSON.stringify(my_anime.value));
 };
 
 const decreaseWatch = (anime) => {
-  anime.watched_evolutions--;
+  anime.watched_episodes--;
   localStorage.setItem("my_anime", JSON.stringify(my_anime.value));
 };
 
@@ -75,19 +75,30 @@ onMounted(() => {
         <img :src="anime.images.jpg.image_url" />
         <div class="details">
           <h3>{{ anime.title }}</h3>
-          <p :title="anime.synopsis" v-if="anime.synopsis">
-            {{ anime.synopsis.slice(0, 120) }}...
-          </p>
+          <p :title="anime.synopsis" v-if="anime.synopsis">{{ anime.synopsis.slice(0, 120) }}...</p>
           <span class="flex-1"></span>
-          <button @click="addAnime(anime)">Add to my anime</button>
+          <button @click="addAnime(anime)" class="button">Add to my anime</button>
         </div>
       </div>
     </div>
 
     <div class="myanime" v-if="my_anime.length > 0">
-          <h2>My Anime</h2>
+      <h2>My Anime</h2>
 
-          <div v-for=""></div>
+      <div v-for="anime in my_anime_asc" class="anime">
+        <img :src="anime.image" />
+        <h3>{{ anime.title }}</h3>
+        <div class="flex-1"></div>
+        <span class="episodes">
+          {{ anime.watched_episodes }} / {{ anime.total_episodes }}
+        </span>
+        <button
+            v-if="anime.total_episodes !== anime.watched_episodes"
+            @click="increaseWatch(anime)">+</button>
+        <button
+            v-if="anime.watched_episodes > 0"
+            @click="decreaseWatch(anime)">-</button>
+      </div>
     </div>
   </main>
 </template>
